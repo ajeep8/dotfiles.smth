88bf7065349bf7699b28bfe34144ccf3ee655f8b
Ajeep <ajeep8@gmail.com>
2024-03-21 15:40:21 +0800

    mv /etc/polybar/launcher.sh to .config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar mybar -c /etc/polybar/config &
+polybar mybar -c ~/.config/polybar/config &
 
 echo "Polybar runing..."
