579d181aadfa97712b9c48448192c55ca253269c
Ajeep <ajeep8@gmail.com>
2024-03-24 15:46:51 +0800

    add wm-select.sh

---
+++ .local/bin/wm-select.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 bspwm|xfwm"
+  exit 1
+fi
+
+if [ "$1" = "xfwm"  ]; then 
+  echo 'xfwm'
+  rm -rf ~/.cache/sessions/xfce4-session-*
+  sed -i 's/^Hidden=false/Hidden=true/' ~/.config/autostart/bspwm.desktop
+  sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.xfwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  reboot
+elif [ "$1" = "bspwm"  ]; then 
+  echo 'bspwm'
+  rm -rf ~/.cache/sessions/xfce4-session-*
+  sed -i 's/^Hidden=true/Hidden=false/' ~/.config/autostart/bspwm.desktop
+  sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.bspwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  reboot
+else
+  echo "Usage: $0 bspwm|xfwm"
+fi
