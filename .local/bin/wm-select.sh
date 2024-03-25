fd83fe52e7e38588a655c23bc526ab9af56e52c5
Ajeep <ajeep8@gmail.com>
2024-03-25 19:11:27 +0800

    use personal session.xml

--- .local/bin/wm-select.sh
+++ .local/bin/wm-select.sh
@@ -9,13 +9,15 @@ if [ "$1" = "xfwm"  ]; then
   echo 'xfwm'
   rm -rf ~/.cache/sessions/xfce4-session-*
   sed -i 's/^Hidden=false/Hidden=true/' ~/.config/autostart/bspwm.desktop
-  sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.xfwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.xfwm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  #sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.xfwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
   reboot
 elif [ "$1" = "bspwm"  ]; then 
   echo 'bspwm'
   rm -rf ~/.cache/sessions/xfce4-session-*
   sed -i 's/^Hidden=true/Hidden=false/' ~/.config/autostart/bspwm.desktop
-  sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.bspwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.bspwm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+  #sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml.bspwm /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
   reboot
 else
   echo "Usage: $0 bspwm|xfwm"
