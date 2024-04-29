daea559c3df700371fff3391f0200d9171e27ca1
Ajeep <ajeep8@gmail.com>
2024-04-29 11:12:10 +0800

    addRef.sh: Section

--- .local/bin/addRef.sh
+++ .local/bin/addRef.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [[ $# -lt 3 ]] ; then
-    echo 'addRef.sh the.md Fig Tbl'
+    echo 'Usage: addRef.sh the.md Fig Tbl'
+    echo "If you want to delete {#fig:Figxxx},{#tbl:Tblxxx}, run: sed -i 's/{#.*:.*}//' filename.md."
+    echo "But if you had refered any of them, the id may change whan re-auto-add next time."
     exit 1
 fi
 
 counter=1
-
 while :
 do
     match=$(grep -m 1 "\!\[.*\](.*)$" "$1")
@@ -20,7 +21,6 @@ do
 done
 
 counter=1
-
 while :
 do
     match=$(grep -m 1 "^Table: [^{}]*$" "$1")
@@ -33,5 +33,18 @@ do
     fi
 done
 
+if [ $4 ]; then
+    counter=1
+    while :
+    do
+        match=$(grep -m 1 "^#\+ [^{}]*$" "$1")
+        echo "$match"
+        if [[ -n "$match" ]]; then
+    	sed -i -E "0,/^#[^{}]*$/s//& {#sec:$4$counter}/" $1
+            ((counter++))
+        else
+            break
+        fi
+    done
+fi
 
-# 如果想删除 {#fig:Figxxx}、{#tbl:Tblxxx}，可运行`sed -i 's/{#.*:.*}$//' 文件名`，但注意，如果已经引用过，再次自动添加后id可能会变
