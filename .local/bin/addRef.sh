34a82f6b3691f9841375a89a0ad1ffa9dab8c5dc
Ajeep <ajeep8@gmail.com>
2024-04-30 17:43:39 +0800

    rewrite addRef.sh

--- .local/bin/addRef.sh
+++ .local/bin/addRef.sh
@@ -1,50 +1,63 @@
 #!/bin/bash
 
+if [ "$1" = "-r" ]; then
+    if [ -n "$2" ] && [ -f "$2" ]; then
+        sed -i 's/ *{#.\+:.\+}//' $2
+        sed -i 's/ *#.\+:.\+}/}/' $2
+    else
+	echo "Usage: addRef.sh -r the.md    # remove {#fig:Figxxx},{#tbl:Tblxxx}"
+    fi
+    exit 0
+fi
+
 if [[ $# -lt 3 ]] ; then
-    echo 'Usage: addRef.sh the.md Fig Tbl'
-    echo "If you want to delete {#fig:Figxxx},{#tbl:Tblxxx}, run: sed -i 's/{#.*:.*}//' filename.md."
+    echo 'Usage: addRef.sh the.md Fig Tbl    # add {#fig:Figxxx},{#tbl:Tblxxx}'
+    echo "Usage: addRef.sh -r the.md    # remove {#fig:Figxxx},{#tbl:Tblxxx}"
     echo "But if you had refered any of them, the id may change whan re-auto-add next time."
     exit 1
 fi
 
+tmp1=$(mktemp)
 counter=1
-while :
-do
-    match=$(grep -m 1 "\!\[.*\](.*)$" "$1")
-    echo "$match"
-    if [[ -n "$match" ]]; then
-	sed -i -E "0,/\!\[.*\]\(.*\)$/s//&{#fig:$2$counter}/" $1
+while IFS= read -r line; do
+    if [[ $line =~ ^!\[.+\](.+)\{[^#]+\} ]]; then
+        modified_line=$(sed -E "s/\}/ #fig:$2$counter\}/" <<< "$line")
+        echo "$modified_line"
+        ((counter++))
+    elif [[ $line =~ ^!\[.+\]([^#]+)$ ]]; then
+        modified_line=$(sed -E "s/$/\{#fig:$2$counter\}/" <<< "$line")
+        echo "$modified_line"
         ((counter++))
     else
-        break
+        echo "$line"
     fi
-done
+done < $1 > $tmp1
 
+tmp=$(mktemp)
 counter=1
-while :
-do
-    match=$(grep -m 1 "^Table: [^{}]*$" "$1")
-    echo "$match"
-    if [[ -n "$match" ]]; then
-	sed -i -E "0,/Table: [^{}]*$/s//& {#tbl:$3$counter}/" $1
+while IFS= read -r line; do
+    if [[ $line =~ Table:\ [^#]+$ ]]; then
+        modified_line=$(sed -E "s/$/ \{#tbl:$3$counter\}/" <<< "$line")
+        echo "$modified_line"
         ((counter++))
     else
-        break
+        echo "$line"
     fi
-done
+done < $tmp1 > $tmp
 
 if [ $4 ]; then
-    counter=1
-    while :
-    do
-        match=$(grep -m 1 "^#\+ [^{}]*$" "$1")
-        echo "$match"
-        if [[ -n "$match" ]]; then
-    	sed -i -E "0,/^#[^{}]*$/s//& {#sec:$4$counter}/" $1
-            ((counter++))
-        else
-            break
-        fi
-    done
+tmp2=$(mktemp)
+cp $tmp $tmp2
+counter=1
+while IFS= read -r line; do
+    if [[ $line =~ ^#+\ +[^#]+$ ]]; then
+        modified_line=$(sed -E "s/$/ \{#sec:$4$counter\}/" <<< "$line")
+        echo "$modified_line"
+        ((counter++))
+    else
+        echo "$line"
+    fi
+done < $tmp2 > $tmp
 fi
 
+mv $tmp $1
